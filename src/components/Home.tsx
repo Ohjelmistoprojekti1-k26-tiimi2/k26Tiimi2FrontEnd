ffc503b5ff9c548ae90f0406a2846456f53bdec2
nadjalyyra <nadjalyyra@gmail.com>
2026-04-28 15:49:22 +0300

    Etusivulle lisätty kuva.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Box } from "@mui/material";
 import Navbar from "./Navbar";
+import etusivuKuva from "../assets/etusivukuva.webp";
 import "./Home.css";
 
 function Home() {
@@ -13,12 +14,20 @@ function Home() {
         <h1>Pikkumussukan koiratarvike</h1>
 
         <p>
-          Kaikkea ihanaa karvaiselle ystävällesi – leluja, herkkuja ja suloisia asusteita.
+          Kaikkea ihanaa karvaiselle ystävällesi: leluja, herkkuja ja suloisia asusteita.
         </p>
 
         <p className="subtext">
           Valitse valikosta mitä haluat tutkia.
         </p>
+      </Box>
+
+      <Box className="bottom-image-container">
+        <img 
+          src={etusivuKuva} 
+          alt="Pikkumussukan koiratarvike" 
+          className="bottom-image"
+        />
       </Box>
 
     </div>
