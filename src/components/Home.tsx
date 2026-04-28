91032c6f89b8408535a9e58fd8bba8ebc43d5b21
nadjalyyra <nadjalyyra@gmail.com>
2026-04-28 12:24:11 +0300

    Etusivun rakenteeseen tehty muutoksia, AppBar lisätty, joka on loogisempi ratkaisu kuin yksittäiset painikkeet.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,22 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import "./Home.css";
 
 function Home() {
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Pikku Mussukan koiratarvike</h1>
-      <p>Valitse toiminto:</p>
+    <>
+      {/* Yläpalkki */}
+      <AppBar position="static">
+        <Toolbar className="toolbar">
+          
+          {/* Logo / nimi */}
+          <Typography variant="h6">
+            🐶 Pikku Mussukan koiratarvike
+          </Typography>
 
-      <Link to="/categories">
-        <button>Selaa tuotteita</button>
-      </Link>
+          {/* Navigaatio */}
+          <div className="nav-buttons">
+            <Button color="inherit" component={Link} to="/categories">
+              Tuotteet
+            </Button>
 
-      <br /><br />
+            <Button color="inherit" component={Link} to="/company">
+              Meistä
+            </Button>
+          </div>
 
-      <Link to="/company">
-        <button>Yrityksen tiedot</button>
-      </Link>
-    </div>
+        </Toolbar>
+      </AppBar>
+
+      {/* Sisältö */}
+      <Box className="content">
+        <Typography variant="h4" gutterBottom>
+          Tervetuloa 🐾
+        </Typography>
+
+        <Typography variant="body1" sx={{ marginBottom: 2 }}>
+          Täältä löydät kaikkea kivaa karvaiselle kaverillesi, leluista herkkuihin ja vaatteisiin!
+        </Typography>
+
+        <Typography variant="body1">
+          Valitse ylhäältä mitä haluat selailla 👆
+        </Typography>
+      </Box>
+    </>
   );
 }
 
