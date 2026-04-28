5cd45ff85130df771cc19b23c4ace4a750304ccb
PeppiVine <peppilottaolivia@gmail.com>
2026-04-28 12:19:33 +0300

    kokeillaan ulkoasua

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Home.css";
 
 function Home() {
   return (
-    <div style={{ padding: "20px" }}>
+    <div className="home-container">
       <h1>Pikku Mussukan koiratarvike</h1>
       <p>Siirry selaamaan tuotteita:</p>
 
