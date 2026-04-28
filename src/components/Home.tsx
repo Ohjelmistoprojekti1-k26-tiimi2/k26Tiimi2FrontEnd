102f891da1c0e7284a6532e616e235f7ceebcca9
nadjalyyra <nadjalyyra@gmail.com>
2026-04-28 12:35:11 +0300

    Etusivun tyyliin tehty muutoksia.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,30 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
 import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
-=======
->>>>>>> 05e706f2de4a6ffc626a4704223ca3188d1e6f05
 import "./Home.css";
 
 function Home() {
   return (
-<<<<<<< HEAD
     <>
-      {/* Yläpalkki */}
       <AppBar position="static">
         <Toolbar className="toolbar">
-          
-          {/* Logo / nimi */}
           <Typography variant="h6">
             🐶 Pikku Mussukan koiratarvike
           </Typography>
-=======
-    <div className="home-container">
-      <h1>Pikku Mussukan koiratarvike</h1>
-      <p>Valitse toiminto:</p>
->>>>>>> 05e706f2de4a6ffc626a4704223ca3188d1e6f05
 
-          {/* Navigaatio */}
           <div className="nav-buttons">
             <Button color="inherit" component={Link} to="/categories">
               Tuotteet
@@ -34,22 +21,20 @@ function Home() {
               Meistä
             </Button>
           </div>
-
         </Toolbar>
       </AppBar>
 
-      {/* Sisältö */}
-      <Box className="content">
+      <Box className="home-container">
         <Typography variant="h4" gutterBottom>
-          Tervetuloa 🐾
+          Tervetuloa
         </Typography>
 
-        <Typography variant="body1" sx={{ marginBottom: 2 }}>
+        <Typography sx={{ marginBottom: 2 }}>
           Täältä löydät kaikkea kivaa karvaiselle kaverillesi, leluista herkkuihin ja vaatteisiin!
         </Typography>
 
-        <Typography variant="body1">
-          Valitse ylhäältä mitä haluat selailla 👆
+        <Typography>
+          Valitse ylhäältä mitä haluat selailla.
         </Typography>
       </Box>
     </>
